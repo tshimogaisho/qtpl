144699398bcb849349db8ec09a88cd28a9db4cc5
gaisho <tshimogaisho@gmail.com>
2012-07-29 10:44:05 +0900

    m

--- app.js
+++ app.js
@@ -27,71 +27,51 @@ app.configure(function(){
 
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));  
-  mongoclient = new mongodb.Db('qtpl', 
-		  new mongodb.Server("127.0.0.1", 27017, {}), {});
-  mongoclient.open(function( err, client){
-		if(err){
-			console.log(err);
-		}else{
-			console.log("connected to mongodb");
-		}
-	});
-
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));  
+	app.use(express.errorHandler());
+	mongoclient = new mongodb.Db('qtpl', 
+			  new mongodb.Server("127.0.0.1", 27017, {}), {});
 });
-
-
 
 app.configure('production', function(){
 	console.log("production....");
 	app.use(express.errorHandler());
 	console.log(process.env.MONGOHQ_URL);
+
 	mongoclient = mongodb.db("mongodb://heroku:herokumongo@flame.mongohq.com:27031/app4012201", function(err){
 		if(err){
 			console.log(err);
 		}
 	});	
-	mongoclient.open(function( err, client){
-		if(err){
-			console.log(err);
-		}else{
-			console.log("connected to mongodb");
-			//test
-			var collection = new mongodb.Collection(mongoclient, 'tpl');
-			collection.findOne({},{},function(err, result){
-				if(err){
-					console.log("connection test. err: ", err);
-				}
-				console.log("connection test. result: ", result);
-			});
-		}
-		
-	});	
 
 });
 
+mongoclient.open(function( err, client){
+	if(err){
+		console.log(err);
+	}else{
+		console.log("connected to mongodb");
+		app.get('/', routes.index);
+		app.get('/:userid', routes.user(client));
+		app.get('/:userid/tree', routes.tree("get", client));
+		app.put('/:userid/tree', routes.tree("put", client));
+		app.post('/:userid/tpl', routes.tpl("post", client));
+		app.get('/:userid/tpl/:nid', routes.tpl("get", client));
+		app.put('/:userid/tpl/:nid', routes.tpl("put", client));
+		app.delete('/:userid/tpl/:nid', routes.tpl("delete", client));
 
-app.get('/', routes.index);
-app.get('/:userid', routes.user(mongoclient));
-app.get('/:userid/tree', routes.tree("get", mongoclient));
-app.put('/:userid/tree', routes.tree("put", mongoclient));
-app.post('/:userid/tpl', routes.tpl("post", mongoclient));
-app.get('/:userid/tpl/:nid', routes.tpl("get", mongoclient));
-app.put('/:userid/tpl/:nid', routes.tpl("put", mongoclient));
-app.delete('/:userid/tpl/:nid', routes.tpl("delete", mongoclient));
+		//app.get('/:userid/tpl/:nid/paramset/', routes.tplparamset("get", client));
+		app.post('/:userid/tpl/:nid/paramset', routes.tplparamset("post", client));
+		app.put('/:userid/tpl/:nid/paramset/:paramsetid', routes.tplparamset("put", client));
+		app.delete('/:userid/tpl/:nid/paramset/:paramsetid', routes.tplparamset("delete", client));
 
-//app.get('/:userid/tpl/:nid/paramset/', routes.tplparamset("get", mongoclient));
-app.post('/:userid/tpl/:nid/paramset', routes.tplparamset("post", mongoclient));
-app.put('/:userid/tpl/:nid/paramset/:paramsetid', routes.tplparamset("put", mongoclient));
-app.delete('/:userid/tpl/:nid/paramset/:paramsetid', routes.tplparamset("delete", mongoclient));
+		app.post('/:userid/tpl/:nid/paramset/:paramsetid/rename', routes.tplparamset.rename("post", client));
 
-app.post('/:userid/tpl/:nid/paramset/:paramsetid/rename', routes.tplparamset.rename("post", mongoclient));
-
-
-app.get('/:userid/testtpls', routes.testtpls);
-
-
-
-var port = process.env.PORT || 3000;
-app.listen(port);
-console.log("Express server listening on port %d in %s mode", port, app.settings.env);
+		app.get('/:userid/testtpls', routes.testtpls);
+		
+		var port = process.env.PORT || 3000;
+		app.listen(port);
+		console.log("Express server listening on port %d in %s mode", port, app.settings.env);		
+	}
+	
+});		
